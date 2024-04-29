007b6646b93b98991429f763a2681fb86754e89b
Rafiul Ilmi <ilmi.mris@gmail.com>
2024-04-29 22:19:31 +0700

    wip: building mris.dev

---
+++ app/article.tsx
@@ -0,0 +1,40 @@
+import type { Work } from "@/.contentlayer/generated";
+import Link from "next/link";
+import { Eye, View } from "lucide-react";
+
+type Props = {
+  work: Work;
+  views: number;
+};
+
+export const Article: React.FC<Props> = ({ work, views }) => {
+  return (
+    <Link href={`/works/${work?.slug}`}>
+      <article className="p-4 md:p-8">
+        <div className="flex justify-between gap-2 items-center">
+          <span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
+            {work?.from ? (
+              <time dateTime={new Date(work?.from).toISOString()}>
+                {Intl.DateTimeFormat(undefined, { dateStyle: "medium" }).format(
+                  new Date(work?.from)
+                )}
+              </time>
+            ) : (
+              <span>SOON</span>
+            )}
+          </span>
+          <span className="text-zinc-500 text-xs  flex items-center gap-1">
+            <Eye className="w-4 h-4" />{" "}
+            {Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
+          </span>
+        </div>
+        <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+          {work?.position}
+        </h2>
+        <p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
+          {work?.company}
+        </p>
+      </article>
+    </Link>
+  );
+};
---
+++ app/works/[slug]/page.tsx
@@ -0,0 +1,48 @@
+import { notFound } from "next/navigation";
+import { allWorks } from "contentlayer/generated";
+import { Mdx } from "@/app/components/mdx";
+import { Header } from "./header";
+import "./mdx.css";
+import { ReportView } from "./view";
+import { Redis } from "@upstash/redis";
+
+export const revalidate = 60;
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+const redis = Redis.fromEnv();
+
+export async function generateStaticParams(): Promise<Props["params"][]> {
+  return allWorks
+    .filter((w) => w.published)
+    .map((w) => ({
+      slug: w.slug,
+    }));
+}
+
+export default async function PostPage({ params }: Props) {
+  const slug = params?.slug;
+  const work = allWorks.find((work) => work.slug === slug);
+
+  if (!work) {
+    notFound();
+  }
+
+  const views =
+    (await redis.get<number>(["pageviews", "works", slug].join(":"))) ?? 0;
+
+  return (
+    <div className="bg-zinc-50 min-h-screen">
+      <Header work={work} views={views} />
+      <ReportView slug={work.slug} />
+
+      <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
+        <Mdx code={work.body.code} />
+      </article>
+    </div>
+  );
+}
