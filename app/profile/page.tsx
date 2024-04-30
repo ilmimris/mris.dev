d7e0aa45870a3e7e1d717c4229ce5b71eb72e97e
Rafiul Ilmi <ilmi.mris@gmail.com>
2024-04-30 21:47:50 +0700

    refactor(profile): skill components

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Card } from "../components/card";
 import { Navigation } from "../components/nav";
+import data from "./data.json";
 
 export default function Page() {
   return (
@@ -18,121 +19,65 @@ export default function Page() {
 
         <div className="mx-auto lg:mx-0">
           <div className="grid grid-cols-1 gap-8 w-full">
-            <div className="w-full">
-              <Card>
-                <div className="flex mx-auto flex-col lg:flex-row items-center">
-                  <div className="p-8">
-                    <div className="relative w-72 h-72 rounded-full overflow-hidden bg-[url('/pp.jpg')] bg-cover bg-right-top bg-no-repeat grayscale hover:grayscale-0"></div>
-                  </div>
-                  <div className="w-full p-8">
-                    <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-                      Muhammad Rafiul Ilmi Syarifudin
-                    </h2>
-                    <h3 className="mt-4 text-xl font-normal text-zinc-100">
-                      Lead Software Engineer | AI & ML Enthusiast
-                    </h3>
-
-                    <p className="mt-4 text-zinc-400">
-                      Results-oriented Backend Software Engineer with over 6
-                      years of experience. Specialized in building high-impact
-                      applications that enhance operational efficiency.
-                      Pioneered the integration of Large Language Models (LLMs)
-                      into Qalboo chatbot, Qarib, significantly improving its
-                      ability to understand and respond to user inquiries. Led
-                      the development of a new attendance monitoring system,
-                      eliminating manual data entry and saving the company 90%
-                      in costs. Spearheaded the creation of an outsourcing
-                      request-fulfillment system, streamlining the recruitment
-                      process and enabling a 90% reduction in hiring time.
-                    </p>
-                    <p className="mt-4 text-zinc-400">
-                      Proven leadership skills in managing teams and guiding
-                      them to achieve project goals. Adept in Web and API
-                      development, with a strong background in Go, Typescript,
-                      Python, MongoDB, RDBMS, Docker, and Gitlab CI. Eager to
-                      bring expertise and leadership abilities to your team,
-                      contributing to mutual success.
-                    </p>
-                    <p className="mt-4 text-zinc-400">
-                      "Success is not measured by lines of code, but by the
-                      problems solved and the value delivered."
-                    </p>
-                  </div>
+            <Card>
+              <div className="flex mx-auto flex-col lg:flex-row items-center">
+                <div className="p-8">
+                  <div className="relative w-72 h-72 rounded-full overflow-hidden bg-[url('/pp.jpg')] bg-cover bg-right-top bg-no-repeat grayscale hover:grayscale-0"></div>
                 </div>
-              </Card>
-            </div>
+                <div className="w-full p-8">
+                  <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+                    Muhammad Rafiul Ilmi Syarifudin
+                  </h2>
+                  <h3 className="mt-4 text-xl font-normal text-zinc-100">
+                    Lead Software Engineer | AI & ML Enthusiast
+                  </h3>
+
+                  <p className="mt-4 text-zinc-400">
+                    Results-oriented Backend Software Engineer with over 6 years
+                    of experience. Specialized in building high-impact
+                    applications that enhance operational efficiency. Pioneered
+                    the integration of Large Language Models (LLMs) into Qalboo
+                    chatbot, Qarib, significantly improving its ability to
+                    understand and respond to user inquiries. Led the
+                    development of a new attendance monitoring system,
+                    eliminating manual data entry and saving the company 90% in
+                    costs. Spearheaded the creation of an outsourcing
+                    request-fulfillment system, streamlining the recruitment
+                    process and enabling a 90% reduction in hiring time.
+                  </p>
+                  <p className="mt-4 text-zinc-400">
+                    Proven leadership skills in managing teams and guiding them
+                    to achieve project goals. Adept in Web and API development,
+                    with a strong background in Go, Typescript, Python, MongoDB,
+                    RDBMS, Docker, and Gitlab CI. Eager to bring expertise and
+                    leadership abilities to your team, contributing to mutual
+                    success.
+                  </p>
+                  <p className="mt-4 text-zinc-400">
+                    "Success is not measured by lines of code, but by the
+                    problems solved and the value delivered."
+                  </p>
+                </div>
+              </div>
+            </Card>
           </div>
         </div>
         <div className="w-full h-px bg-zinc-800" />
         <h2 className="mt-4 text-2xl font-normal text-zinc-100">Skills</h2>
 
-        <div className="w-full grid grid-cols-3 gap-8">
-          <Card>
-            <div className="p-8 font-normal text-zinc-100">
-              <h3 className="mt-4 text-xl">Web development</h3>
-              <ul className="font-light text-zinc-400">
-                <li>Go</li>
-                <li>Python</li>
-                <li>NodeJS</li>
-                <li>GraphQL</li>
-                <li>ReactJS</li>
-              </ul>
-            </div>
-          </Card>
-
-          <Card>
-            <div className="p-8 font-normal text-zinc-100">
-              <h3 className="mt-4 text-xl">Databases</h3>
-              <ul className="font-light text-zinc-400">
-                <li>MongoDB</li>
-                <li>Postgres</li>
-                <li>Redis</li>
-                <li>Neo4J</li>
-              </ul>
-            </div>
-          </Card>
-
-          <Card>
-            <div className="p-8 font-normal text-zinc-100">
-              <h3 className="mt-4 text-xl">Message Broker</h3>
-              <ul className="font-light text-zinc-400">
-                <li>Kafka</li>
-              </ul>
-            </div>
-          </Card>
-
-          <Card>
-            <div className="p-8 font-normal text-zinc-100">
-              <h3 className="mt-4 text-xl">AI</h3>
-              <ul className="font-light text-zinc-400">
-                <li>LLMs</li>
-                <li>OpenAI</li>
-                <li>Tensorflow 2 and Pytorch</li>
-              </ul>
-            </div>
-          </Card>
-
-          <Card>
-            <div className="p-8 font-normal text-zinc-100">
-              <h3 className="mt-4 text-xl">Cloud</h3>
-              <ul className="font-light text-zinc-400">
-                <li>Kubernetes</li>
-                <li>GCP</li>
-                <li>AWS</li>
-              </ul>
-            </div>
-          </Card>
-
-          <Card>
-            <div className="p-8 font-normal text-zinc-100">
-              <h3 className="mt-4 text-xl">DevOps</h3>
-              <ul className="font-light text-zinc-400">
-                <li>Docker</li>
-                <li>Gitlab CI</li>
-                <li>Terraform</li>
-              </ul>
-            </div>
-          </Card>
+        <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-8">
+          {data.map((item) => (
+            <Card key={item?.id}>
+              <div className="p-8 font-normal text-zinc-100">
+                <h3 className="mt-4 text-xl">{item?.category}</h3>
+                <ul className="font-light text-zinc-400">
+                  {item?.skills?.map((skill) => (
+                    <li key={skill}>{skill}</li>
+                  ))}
+                </ul>
+              </div>
+            </Card>
+          ))}
         </div>
       </div>
     </div>
