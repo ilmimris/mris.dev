c4cc83a68b67bf03f2c4780c3a90083304f80c3b
Rafiul Ilmi <ilmi.mris@gmail.com>
2025-02-10 23:37:56 +0700

    docs: add more projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,9 +20,9 @@ export default async function ProjectsPage() {
 		return acc;
 	}, {} as Record<string, number>);
 
-	const featured = allProjects.find((project) => project?.slug === "mris.dev")!;
-	const top2 = allProjects.find((project) => project?.slug === "planetfall")!;
-	const top3 = allProjects.find((project) => project?.slug === "highstorm")!;
+	const featured = allProjects.find((project) => project?.slug === "redshiftdnn")!;
+	const top2 = allProjects.find((project) => project?.slug === "covid19")!;
+	const top3 = allProjects.find((project) => project?.slug === "mris.dev")!;
 	const sorted = allProjects
 		.filter((p) => p.published)
 		.filter(
